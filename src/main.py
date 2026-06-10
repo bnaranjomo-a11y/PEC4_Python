19579fd8473a13294faf5c40e43d41ecaca14cc9
bnaranjomo-a11y <bnaranjomo@uoc.edu>
2026-06-10 21:04:53 +0200

    Add header comments and main function to main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
+"""
+PEC4 Programción para la Ciencia de Datos
+Alumno: Brandon Naranjo 
+"""
 
+def main():
+     print("PEC4 Programación para la Ciencia de Datos")
+     print("Alumno: Brandon Naranjo")
+if __name__=="__main__":
+     main()
