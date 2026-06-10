b5c303e5ae56588047ff537f9bc82574134c2cfc
bnaranjomo-a11y <bnaranjomo@uoc.edu>
2026-06-10 22:50:47 +0200

    Implement load_and_eda function for dataset loading
    Adds a function to load a CSV file and perform EDA.

--- src/exercises/ex1.py
+++ src/exercises/ex1.py
@@ -1,1 +1,16 @@
+import pandas as pd
+
+def load_and_eda(file: str) -> pd.DataFrame:
+  """
+  La función carga el dataset y elimina las columnas innecesarias y muestra la información básica.
+  Args:
+  file (str): Ruta del arcchivo CSV
+  Returns:
+  pd.DataFrame: Dataset cargado sin las columnas HTHG, HTAG, HTR,
+  """
+#cargamos el archivo csv utilizando la ruta establecida
+#mediante "axis1" indicamos que eliminamos las columnas "HTHG","HTAG","HTR"
+  data=pd.read_csv(file)
+  data=data.drop(["HTHG","HTAG","HTR"],axis=1)
+  return data
 
