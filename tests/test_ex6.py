d3e54c21209a087b3a390a0ad8dbd90ba03e4dae
bnaranjomo-a11y <bnaranjomo@uoc.edu>
2026-06-11 21:56:38 +0200

    Update test_ex6.py

--- tests/test_ex6.py
+++ tests/test_ex6.py
@@ -1,1 +1,12 @@
+import unittest
+import pandas as pd
 
+from src.exercises.ex6 import fun_total_goals
+class TestFunTotalGoals(unittest.TestCase):
+   def test_fun_total_goals(self):
+     data=pd.DataFrame({"FTHG":[2,1,3],"FTAG":[1,0,2]})
+     resultado=fun_total_goals(data)
+     self.asserEqual(resultado,/6,3,9))
+
+if __name__=="__main__":
+  unittest.main()
